b6fa9d43a709c2c1a123dedc7d19f40f1df9bfaf
Jon Moniaci <jonmoniaci@gmail.com>
2021-06-29 14:30:00 -0400

    Control Envelope written and tested

---
+++ src/control_envelope.js
@@ -0,0 +1,39 @@
+export default class {
+  // timeData is a hash with keys of seconds ints,
+  // values of the envelope value. time should start
+  // at 0
+  // { x1: y1, x2: y2 }
+  constructor(envelope) {
+    this.envelope = envelope;
+    this.times = Object.keys(envelope).sort((a, b) => a - b).map(val => parseInt(val, 10));
+  }
+
+  // sample the envelope at the given time (x)
+  sample(time) {
+    for (let i = 0; i < this.times.length; i += 1) {
+      const lastTime = this.times[i];
+      const lastValue = this.envelope[lastTime];
+
+      if (time === lastTime) {
+        return lastValue;
+      }
+
+      const nextTime = this.times[i + 1];
+
+      if (nextTime === undefined || nextTime === null) {
+        throw new Error('nextTime is not defined');
+      }
+
+      if (time > lastTime && time < nextTime) {
+
+        const nextValue = this.envelope[nextTime];
+
+        const valueDelta = nextValue - lastValue;
+        const timeDelta = nextTime - lastTime;
+        const sampleTimeDelta = time - lastTime;
+
+        return ((sampleTimeDelta * valueDelta) / timeDelta) + lastValue;
+      }
+    };
+  }
+}
---
+++ src/control_envelope.test.js
@@ -0,0 +1,20 @@
+import Envelope from './control_envelope.js';
+
+test('samples the time correctly', () => {
+  const env = new Envelope({
+    0: 0,
+    10: 10,
+    15: 11,
+  });
+
+  expect(env.sample(0)).toBe(0);
+  expect(env.sample(5)).toBe(5);
+  expect(env.sample(11)).toBe(10.2);
+  expect(env.sample(12)).toBe(10.4);
+  expect(env.sample(15)).toBe(11);
+
+  const t = () => {
+    env.sample(15.1);
+  }
+  expect(t).toThrow('nextTime is not defined');
+});
